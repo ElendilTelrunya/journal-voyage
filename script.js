a8272efc430969d180d8e39ff6babbeb50cd0184
ElendilTelrunya <clem63470@gmail.com>
2026-03-08 12:17:02 +0100

    Refactor map layer and improve country click handling

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ menu.classList.toggle("open");
 }
 
 function showPage(page){
-
 document.querySelectorAll(".page").forEach(p=>{
 p.style.display="none";
 });
@@ -12,78 +11,44 @@ p.style.display="none";
 if(page==="map"){
 document.getElementById("mapPage").style.display="block";
 }
-
 if(page==="albums"){
 document.getElementById("albumsPage").style.display="block";
 }
-
 if(page==="concerts"){
 document.getElementById("concertsPage").style.display="block";
 }
-
 }
 
+// Carte interactive
 var map = L.map('map').setView([20,0],2);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{ maxZoom:19 }).addTo(map);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-maxZoom:19
-}).addTo(map);
-
-
+// Pays visités
 let visitedCountries = JSON.parse(localStorage.getItem("visitedCountries")) || {};
 
 fetch("https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson")
 .then(res => res.json())
 .then(data => {
-
 L.geoJSON(data,{
-
 style:function(feature){
-
 let name = feature.properties.ADMIN;
-
 if(visitedCountries[name]){
-
-return{
-color:"#555",
-fillColor:"#999",
-fillOpacity:0.7
-};
-
+return{ color:"#555", fillColor:"#999", fillOpacity:0.7 };
 }
-
-return{
-color:"#333",
-fillColor:"#ccc",
-fillOpacity:0.2
-};
-
+return{ color:"#333", fillColor:"#ccc", fillOpacity:0.2 };
 },
-
 onEachFeature:function(feature,layer){
-
 let name = feature.properties.ADMIN;
-
 layer.on("click",function(){
-
 visitedCountries[name] = true;
-
-localStorage.setItem(
-"visitedCountries",
-JSON.stringify(visitedCountries)
-);
-
-layer.setStyle({
-fillColor:"#999",
-fillOpacity:0.7
+localStorage.setItem("visitedCountries", JSON.stringify(visitedCountries));
+layer.setStyle({ fillColor:"#999", fillOpacity:0.7 });
 });
-
-});
-
 layer.bindPopup(name);
-
 }
-
 }).addTo(map);
-
 });
+
+// Fonctions vides pour albums / concerts pour l'instant
+function addCountry(){ alert("Ajout pays prochainement"); }
+function addConcert(){ alert("Ajout concert prochainement"); }
