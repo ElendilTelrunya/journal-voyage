92c09f2a4b6810e29e156071eddbc9f0c45bcd32
ElendilTelrunya <clem63470@gmail.com>
2026-03-09 22:57:54 +0100

    Refactor login, menu, and media handling
    Refactor login functionality and improve menu handling. Update media handling and add category management features.

--- script.js
+++ script.js
@@ -1,237 +1,119 @@
-const PASSWORD="Cybelle-1"
-
-let map
-let markers=[]
-
+const PASSWORD="voyage";
+let visitedCountries = JSON.parse(localStorage.getItem("visitedCountries")) || {};
 
 function login(){
-
-let p=document.getElementById("password").value
-
-if(p===PASSWORD){
-
-document.getElementById("login").style.display="none"
-document.getElementById("site").style.display="block"
-
-init()
-
-}else{
-
-alert("Mauvais mot de passe")
-
+  if(document.getElementById("password").value === PASSWORD){
+    document.getElementById("login").style.display="none";
+    document.getElementById("site").style.display="block";
+    init();
+  } else alert("Mauvais mot de passe");
 }
 
-}
-
-
-function toggleMenu(){
-
-document.getElementById("menu").classList.toggle("open")
-
-}
-
-function closeMenu(){
-
-document.getElementById("menu").classList.remove("open")
-
-}
-
+function toggleMenu(){document.getElementById("menu").classList.toggle("open");}
+function closeMenu(){document.getElementById("menu").classList.remove("open");}
 
 function init(){
-
-initMap()
-loadGallery()
-
+  showHome();
+  loadGallery();
+  loadCategories();
+  initSVGMap();
 }
 
-
-/* NAVIGATION */
-
-function showHome(){
-
-document.getElementById("homePage").classList.remove("hidden")
-document.getElementById("mapPage").classList.add("hidden")
-document.getElementById("categoryPage").classList.add("hidden")
-
-closeMenu()
-
+function showHome(){ showSection("homePage") }
+function showMap(){ showSection("mapPage") }
+function showCategories(){ showSection("categoryPage") }
+function showSection(id){
+  ["homePage","mapPage","categoryPage"].forEach(s => document.getElementById(s).classList.add("hidden"));
+  document.getElementById(id).classList.remove("hidden");
+  closeMenu();
 }
 
-function showMap(){
-
-document.getElementById("homePage").classList.add("hidden")
-document.getElementById("mapPage").classList.remove("hidden")
-document.getElementById("categoryPage").classList.add("hidden")
-
-closeMenu()
-
+/* svgMap */
+function initSVGMap(){
+  let svgmap = new svgMap({
+    targetElementID: "svgMap",
+    data: {
+      data: {
+        visited: {
+          name: "Visité",
+          format: "{0}",
+          svgClass: "visited",
+          value: "non"
+        }
+      },
+      applyData: "visited",
+      values: visitedCountries
+    },
+    interactive: true,
+    onCountryClick: function(countryCode){
+      visitedCountries[countryCode] = visitedCountries[countryCode] === "oui" ? "non" : "oui";
+      localStorage.setItem("visitedCountries", JSON.stringify(visitedCountries));
+      svgmap.updateChoropleth(visitedCountries);
+    }
+  });
 }
 
-function showCategories(){
-
-document.getElementById("homePage").classList.add("hidden")
-document.getElementById("mapPage").classList.add("hidden")
-document.getElementById("categoryPage").classList.remove("hidden")
-
-closeMenu()
-
+/* CATEGORIES */
+function addCategory(){
+  let name=document.getElementById("newCategory").value.trim();
+  let cats=JSON.parse(localStorage.getItem("cats"))||{};
+  if(name && !cats[name]) cats[name]=[];
+  localStorage.setItem("cats",JSON.stringify(cats));
+  loadCategories();
 }
 
-
-
-/* CARTE */
-
-function initMap(){
-
-map=L.map('map').setView([20,0],2)
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
-
-let saved=JSON.parse(localStorage.getItem("markers"))||[]
-
-saved.forEach(addMarker)
-
-map.on("click",function(e){
-
-addMarker([e.latlng.lat,e.latlng.lng])
-
-saveMarkers()
-
-})
-
+function addSub(){
+  let parent=document.getElementById("parentCategory").value;
+  let sub=document.getElementById("newSub").value.trim();
+  let cats=JSON.parse(localStorage.getItem("cats"))||{};
+  if(parent && sub) cats[parent].push(sub);
+  localStorage.setItem("cats",JSON.stringify(cats));
+  loadCategories();
 }
 
-
-function addMarker(coords){
-
-let marker=L.circle(coords,{
-radius:500000,
-color:"grey",
-fillColor:"grey",
-fillOpacity:0.5
-}).addTo(map)
-
-marker.on("click",function(){
-
-map.removeLayer(marker)
-
-markers=markers.filter(m=>m!==marker)
-
-saveMarkers()
-
-})
-
-markers.push(marker)
-
+function loadCategories(){
+  let cats=JSON.parse(localStorage.getItem("cats"))||{};
+  let ul=document.getElementById("categories"), select=document.getElementById("parentCategory");
+  ul.innerHTML=""; select.innerHTML="";
+  for(let c in cats){
+    let li=document.createElement("li"); li.innerText=c;
+    li.onclick=()=>{ alert("Catégorie : "+c); closeMenu(); }
+    ul.appendChild(li);
+    let opt=document.createElement("option"); opt.value=c; opt.innerText=c; select.appendChild(opt);
+    cats[c].forEach(s => {
+      let sub=document.createElement("li"); sub.innerText=" - "+s;
+      sub.onclick=()=>{ alert("Sous-catégorie : "+s); closeMenu(); }
+      ul.appendChild(sub);
+    });
+  }
 }
-
-
-function saveMarkers(){
-
-let data=markers.map(m=>[m.getLatLng().lat,m.getLatLng().lng])
-
-localStorage.setItem("markers",JSON.stringify(data))
-
-}
-
-
 
 /* MEDIA */
-
 function addMedia(){
-
-let input=document.createElement("input")
-
-input.type="file"
-
-input.accept="image/*,video/*"
-
-input.onchange=e=>{
-
-let file=e.target.files[0]
-
-let reader=new FileReader()
-
-reader.onload=function(){
-
-let media=JSON.parse(localStorage.getItem("media"))||[]
-
-media.push(reader.result)
-
-localStorage.setItem("media",JSON.stringify(media))
-
-loadGallery()
-
+  let input=document.createElement("input"); input.type="file"; input.accept="image/*,video/*";
+  input.onchange=e=>{
+    let reader=new FileReader();
+    reader.onload=function(){
+      let media=JSON.parse(localStorage.getItem("media"))||[];
+      media.push(reader.result);
+      localStorage.setItem("media",JSON.stringify(media));
+      loadGallery();
+    }
+    reader.readAsDataURL(e.target.files[0]);
+  }
+  input.click();
 }
-
-reader.readAsDataURL(file)
-
-}
-
-input.click()
-
-}
-
 
 function loadGallery(){
-
-let gallery=document.getElementById("gallery")
-
-gallery.innerHTML=""
-
-let media=JSON.parse(localStorage.getItem("media"))||[]
-
-media.forEach((m,i)=>{
-
-let div=document.createElement("div")
-
-div.className="media"
-
-let del=document.createElement("button")
-
-del.className="delete"
-
-del.innerText="X"
-
-del.onclick=()=>deleteMedia(i)
-
-if(m.includes("video")){
-
-let v=document.createElement("video")
-
-v.src=m
-
-v.controls=true
-
-div.appendChild(v)
-
-}else{
-
-let img=document.createElement("img")
-
-img.src=m
-
-div.appendChild(img)
-
-}
-
-div.appendChild(del)
-
-gallery.appendChild(div)
-
-})
-
-}
-
-
-function deleteMedia(i){
-
-let media=JSON.parse(localStorage.getItem("media"))
-
-media.splice(i,1)
-
-localStorage.setItem("media",JSON.stringify(media))
-
-loadGallery()
-
+  let gallery=document.getElementById("gallery"); gallery.innerHTML="";
+  JSON.parse(localStorage.getItem("media"))?.forEach((m,i)=>{
+    let div=document.createElement("div"); div.className="media";
+    let del=document.createElement("button"); del.className="delete"; del.innerText="X"; del.onclick=()=>{
+      let media=JSON.parse(localStorage.getItem("media")); media.splice(i,1);
+      localStorage.setItem("media",JSON.stringify(media)); loadGallery();
+    }
+    if(m.includes("video")){let v=document.createElement("video"); v.src=m; v.controls=true; div.appendChild(v);}
+    else{let img=document.createElement("img"); img.src=m; div.appendChild(img);}
+    div.appendChild(del); gallery.appendChild(div);
+  });
 }
